2865ea9d123a1f700cba36d99471c4519c1993a4
Andor <andorhorvat@gmail.com>
2020-02-22 11:49:02 +0100

    User and name sorted

--- src/User.vue
+++ src/User.vue
@@ -1,13 +1,44 @@
 <template>
     <div>
       <p>User.vue</p>
+      <p>Name: {{ name }}</p>
+      <p>Age: {{ age }}</p>
+      <button @click="changeToMerlin" class="btn btn-info">Change name to Merlin</button>
+      <button @click="changeToArthur" class="btn btn-info">Change name to Arthur</button>
+      <hr>
+      <userdetail-tag></userdetail-tag>
+      <useredit-tag></useredit-tag>
     </div>
 </template>
 
 <script>
-    export default {
+  import UserDetail from './UserDetail.vue';
+  import UserEdit from './UserEdit.vue';
+  
+  export default {
 
+    data: function(){
+      return {
+        name: 'Arthur',
+        age: 29,
+      }
+    },
+
+    methods: {
+      changeToMerlin(){
+        this.name = 'Merlin';
+      },
+
+      changeToArthur(){
+        this.name = 'Arthur';
+      }
+    },
+
+    components: {
+      'useredit-tag': UserEdit,
+      'userdetail-tag': UserDetail,
     }
+  }
 </script>
 
 <style>
